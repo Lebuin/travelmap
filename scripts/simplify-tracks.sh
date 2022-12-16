05736cd488b9728fc312b0c3bb3b31b3790288e0
Seppe Lenders <seppe.lenders@gmail.com>
2022-12-16 15:31:25 +0100

    add transit to kopoenhagen

--- scripts/simplify-tracks.sh
+++ scripts/simplify-tracks.sh
@@ -1,11 +1,12 @@
 cd src/assets/tracks
 
-mkdir -p ../../../dist/assets/tracks/0
+rm -rf ../../../dist/assets/tracks
+mkdir -p ../../../dist/assets/tracks
 mkdir -p ../../../dist/assets/tracks/0.01
 mkdir -p ../../../dist/assets/tracks/0.001
+ln -s ../../../../src/assets/tracks ../../../dist/assets/tracks/0
 
 for f in *.json; do
-  cp $f ../../../dist/assets/tracks/0/$f
   cat $f | npx simplify-geojson -t 0.01 > ../../../dist/assets/tracks/0.01/$f
   cat $f | npx simplify-geojson -t 0.001 > ../../../dist/assets/tracks/0.001/$f
 done
