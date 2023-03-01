df406cf079010004ad900f5531254ea4ab3d91c1
Seppe Lenders <seppe.lenders@gmail.com>
2023-03-01 10:56:09 +0100

    add transport to italie-balkan

--- scripts/simplify-tracks.sh
+++ scripts/simplify-tracks.sh
@@ -7,6 +7,7 @@ mkdir -p ../../../dist/assets/tracks/0.001
 ln -s ../../../src/assets/tracks ../../../dist/assets/tracks/0
 
 for f in *.json; do
+  echo $f
   cat $f | npx simplify-geojson -t 0.01 > ../../../dist/assets/tracks/0.01/$f
   cat $f | npx simplify-geojson -t 0.001 > ../../../dist/assets/tracks/0.001/$f
 done
