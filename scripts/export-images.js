7766fd740764337d8527723e61e872133af7765c
Seppe Lenders <seppe.lenders@gmail.com>
2023-09-24 09:45:57 +0200

    stitch panoramas in export-images.js

--- scripts/export-images.js
+++ scripts/export-images.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-const { parse } = require('csv-parse/sync');
 const fs = require('fs');
 const path = require('path');
-const { execSync } = require('child_process');
+const glob = require('glob');
+const { parse } = require('csv-parse/sync');
+const { execFileSync } = require('child_process');
 
 const PATH_TRAVEL_DEFS = 'src/assets/travels.csv';
 const BASE_FOLDER_IMAGES_SRC_LOCAL = '/home/seppe/Pictures';
@@ -14,6 +15,18 @@ const IMAGE_SIZE = 2000;
 const IMAGE_QUALITY = 90;
 
 
+function main() {
+  const travelDefs = readTravelDefs(PATH_TRAVEL_DEFS);
+  const travelIds = process.argv.length > 2 ?
+    process.argv.slice(2) :
+    travelDefs.map(travelDef => travelDef.id);
+  travelDefs
+    .filter(travelDef => travelIds.includes(travelDef.id))
+    .filter(travelDef => travelDef.imageFolder)
+    .forEach(exportImages);
+}
+
+
 function readTravelDefs(path) {
   const content = fs.readFileSync(path);
   const travelDefs = parse(content, {
@@ -21,6 +34,28 @@ function readTravelDefs(path) {
     skipEmptyLines: true,
   });
   return travelDefs;
+}
+
+
+function exportImages(travelDef) {
+  console.log(travelDef.id);
+
+  const srcFolder = getSrcFolder(travelDef);
+  const destFolder = getDestFolder(travelDef);
+
+  const destFolderTmp = destFolder + '.tmp';
+  if(fs.existsSync(destFolderTmp)) {
+    fs.rmSync(destFolderTmp, { recursive: true });
+  }
+  fs.mkdirSync(destFolderTmp);
+
+  exportDarktable(srcFolder, destFolderTmp);
+  stitchPanoramas(srcFolder, destFolderTmp);
+
+  if(fs.existsSync(destFolder)) {
+    fs.rmSync(destFolder, { recursive: true });
+  }
+  fs.renameSync(destFolderTmp, destFolder);
 }
 
 
@@ -38,35 +73,45 @@ function getDestFolder(travelDef) {
 }
 
 
-function exportImages(travelDef) {
-  const srcFolder = getSrcFolder(travelDef);
-  const destFolder = getDestFolder(travelDef);
-  if(fs.existsSync(destFolder)) {
-    return;
-  }
 
-  console.log(travelDef.id);
-  const destFolderTmp = destFolder + '.tmp';
-  if(fs.existsSync(destFolderTmp)) {
-    fs.rmSync(destFolderTmp, { recursive: true });
-  }
-
-  fs.mkdirSync(destFolderTmp);
-  const command = `darktable-cli '${srcFolder}' '${destFolderTmp}' --width ${IMAGE_SIZE} --height ${IMAGE_SIZE} --core --conf plugins/imageio/format/jpeg/quality=${IMAGE_QUALITY}`;
-  execSync(command);
-  fs.renameSync(destFolderTmp, destFolder);
+function exportDarktable(srcFolder, destFolder) {
+  execFileSync('darktable-cli', [
+    srcFolder,
+    destFolder,
+    '--width', IMAGE_SIZE,
+    '--height', IMAGE_SIZE,
+    '--core',
+    '--conf', `plugins/imageio/format/jpeg/quality=${IMAGE_QUALITY}`,
+  ]);
 }
 
 
-function main() {
-  const travelDefs = readTravelDefs(PATH_TRAVEL_DEFS);
-  const travelIds = process.argv.length > 2 ?
-    process.argv.slice(2) :
-    travelDefs.map(travelDef => travelDef.id);
-  travelDefs
-    .filter(travelDef => travelIds.includes(travelDef.id))
-    .filter(travelDef => travelDef.imageFolder)
-    .forEach(exportImages);
+
+function stitchPanoramas(srcFolder, destFolder) {
+  const srcPtoFiles = findPtoFiles(srcFolder);
+  const destPtoFiles = copyPtoFiles(srcPtoFiles, destFolder);
+
+  execFileSync('hugin-batch', destPtoFiles);
+
+  destPtoFiles.forEach(destPtoFile => {
+    fs.rmSync(destPtoFile);
+  });
 }
+
+
+function findPtoFiles(folder) {
+  const pattern = `${folder}*/*.pto`;
+  return glob.sync(pattern);
+}
+
+function copyPtoFiles(srcPtoFiles, destFolder) {
+  const destPtoFiles = srcPtoFiles.map(srcPtoFile => {
+    const destPtoFile = path.join(destFolder, path.basename(srcPtoFile));
+    fs.copyFileSync(srcPtoFile, destPtoFile);
+    return destPtoFile;
+  });
+  return destPtoFiles;
+}
+
 
 main();
