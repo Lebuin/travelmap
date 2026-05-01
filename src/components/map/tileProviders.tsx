41382004f88df0cad57d9bfed12824ea5c323dcf
Seppe Lenders <seppe.lenders@gmail.com>
2026-05-01 15:31:03 +0200

    store mapbox api token in .env

--- src/components/map/tileProviders.tsx
+++ src/components/map/tileProviders.tsx
@@ -1,10 +1,7 @@
 const OSM_ATTRIBUTION: string =
   '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
-
 const MAPBOX_ATTRIBUTION: string =
   '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a>';
-const MAPBOX_ACCESS_TOKEN: string =
-  'pk.eyJ1IjoibGVidWluIiwiYSI6ImNpcG90ZGRsODAwMmZoem5iejVieGNrdjkifQ.vb73zLJdMoc8WiyYLk4SCg';
 
 export interface TileProvider {
   name: string;
@@ -17,7 +14,7 @@ const tileProviders: Array<TileProvider> = [
     name: 'Mapbox',
     url:
       'https://a.tiles.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=' +
-      MAPBOX_ACCESS_TOKEN,
+      process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
     attribution: [OSM_ATTRIBUTION, MAPBOX_ATTRIBUTION].join(' '),
   },
   {
